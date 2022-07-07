e40a5e840fd4c07883efeea2ffcab143d6815e3a
Tagir <khalilov.tg@gmail.com>
2022-07-08 02:49:06 +0300

    Add higlighting of selected genres items in menu

--- gamesite/gameinfo/templatetags/gameinfo_tags.py
+++ gamesite/gameinfo/templatetags/gameinfo_tags.py
@@ -4,15 +4,18 @@ from gameinfo.models import *
 register = template.Library()
 
 @register.inclusion_tag('gameinfo/inclusiontemps/genre_list.html')
-def showGenreList() -> dict:
+def showGenreList(selectedId = None) -> dict:
     genres = dict(
         zip(
             [genre.name for genre in Genre.objects.all().order_by('id')],
             [genre.name_loc for genre in Genre.objects.all().order_by('id')]
         )
     )
-
-    return {'genres': genres}
+    if selectedId == None:
+        return { 'genres': genres, 'selected': '-' }
+    else:
+        return { 'genres': genres, 'selected': Genre.objects.get(name_loc=selectedId).name }
+   
 
 @register.inclusion_tag('gameinfo/inclusiontemps/genre_info_games.html')
 def showGenreGames(genreId: int) -> dict:
